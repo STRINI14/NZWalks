573aa00a6c18a4f7fbac3d486d89c337a1640f50
strini <naicker.strini7@gmail.com>
2024-05-17 19:51:25 +0200

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    //https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentnames = new string[] { "John", "Jane", "Jim", "James" };
+
+            return Ok(studentnames);
+        }
+    }
+}
---
+++ NZWalks.API/Model/DTO/AddRegionRequestDto.cs
@@ -0,0 +1,13 @@
+﻿namespace NZWalks.API.Model.DTO
+{
+    public class AddRegionRequestDto
+    {
+        public string? Code { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? RegionImageUrl { get; set; }
+
+
+    }
+}
